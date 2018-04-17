5e98221aec36df8125148a998de44301ecdd090f
Andrew Khloponin <cryptodoge@hospserver.com>
2018-04-17 17:53:16 +0300

    Add graphs

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# CryptoCompare API Tutorial
-These examples show how you can use the CryptoCompare API.
+# CryptoDoge
+This is modified version of [cryptoqween.github.io](https://cryptoqween.github.io/) used only for personal usage.
 
+As cryptoqween we use CryptoCompare API
 Read official documentation [here](https://www.cryptocompare.com/api).
-
-Include ccc-streamer-utilities.js file to your project to get utility functions.
-
-## Content
 
 ### Streamer API
 1. [Trade streamer example](https://cryptoqween.github.io/streamer/trade/)
--- stream.js
+++ stream.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-
 	var currentPrice = {};
 	var socket = io.connect('https://streamer.cryptocompare.com/');
 	//Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
 	//Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
 	//For aggregate quote updates use CCCAGG as market
 	var subscription = ['5~CCCAGG~DOGE~USD', '5~CCCAGG~DOGE~BTC', '5~CCCAGG~BTC~USD', '5~CCCAGG~ETH~USD'];
-	socket.emit('SubAdd', { subs: subscription });
+	socket.emit('SubAdd', {
+		subs: subscription
+	});
 	socket.on("m", function(message) {
 		var messageType = message.substring(0, message.indexOf("~"));
 		var res = {};
@@ -16,62 +17,105 @@ $(document).ready(function() {
 		}
 	});
 
-	var dataUnpack = function(data) {
-		var from = data['FROMSYMBOL'];
-		var to = data['TOSYMBOL'];
-		var fsym = CCC.STATIC.CURRENCY.getSymbol(from);
-		var tsym = CCC.STATIC.CURRENCY.getSymbol(to);
-		var pair = from + to;
-		console.log(data);
-
-		if (!currentPrice.hasOwnProperty(pair)) {
-			currentPrice[pair] = {};
-		}
-
-		for (var key in data) {
-			currentPrice[pair][key] = data[key];
-		}
-
-		if (currentPrice[pair]['LASTTRADEID']) {
-			currentPrice[pair]['LASTTRADEID'] = parseInt(currentPrice[pair]['LASTTRADEID']).toFixed(0);
-		}
-		currentPrice[pair]['CHANGE24HOUR'] = CCC.convertValueToDisplay(tsym, (currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']));
-		currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";;
-		displayData(currentPrice[pair], from, to, tsym, fsym);
-	};
-
-	var displayData = function(current, from, to, tsym, fsym) {
-		console.log(current);
-		var priceDirection = current.FLAGS;
-		for (var key in current) {
-			if (key == 'CHANGE24HOURPCT') {
-				$('#' + key + '_' + from + '_' + to).text(' (' + current[key] + ')');
-			}
-			else if (key == 'LASTVOLUMETO' || key == 'VOLUME24HOURTO') {
-				$('#' + key + '_' + from + '_' + to).text(CCC.convertValueToDisplay(tsym, current[key]));
-			}
-			else if (key == 'LASTVOLUME' || key == 'VOLUME24HOUR' || key == 'OPEN24HOUR' || key == 'OPENHOUR' || key == 'HIGH24HOUR' || key == 'HIGHHOUR' || key == 'LOWHOUR' || key == 'LOW24HOUR') {
-				$('#' + key + '_' + from + '_' + to).text(CCC.convertValueToDisplay(fsym, current[key]));
-			}
-			else {
-				$('#' + key + '_' + from + '_' + to).text(current[key]);
-			}
-		}
-
-		$('#PRICE_' + from).removeClass();
-		if (priceDirection & 1) {
-			$('#PRICE_' + from + '_' + to).addClass("up");
-		}
-		else if (priceDirection & 2) {
-			$('#PRICE_' + from + '_' + to).addClass("down");
-		}
-		if (current['PRICE'] > current['OPEN24HOUR']) {
-			$('#CHANGE24HOURPCT_' + from + '_' + to).removeClass();
-			$('#CHANGE24HOURPCT_' + from + '_' + to).addClass("up");
-		}
-		else if (current['PRICE'] < current['OPEN24HOUR']) {
-			$('#CHANGE24HOURPCT_' + from + '_' + to).removeClass();
-			$('#CHANGE24HOURPCT_' + from + '_' + to).addClass("down");
-		}
-	};
+	square('container');
 });
+
+function displayData(current, from, to, tsym, fsym) {
+	console.log(current);
+	var priceDirection = current.FLAGS;
+	for (var key in current) {
+		if (key == 'CHANGE24HOURPCT') {
+			$('#' + key + '_' + from + '_' + to).text(' (' + current[key] + ')');
+		} else if (key == 'LASTVOLUMETO' || key == 'VOLUME24HOURTO') {
+			$('#' + key + '_' + from + '_' + to).text(CCC.convertValueToDisplay(tsym, current[key]));
+		} else if (key == 'LASTVOLUME' || key == 'VOLUME24HOUR' || key == 'OPEN24HOUR' || key == 'OPENHOUR' || key == 'HIGH24HOUR' || key == 'HIGHHOUR' || key == 'LOWHOUR' || key == 'LOW24HOUR') {
+			$('#' + key + '_' + from + '_' + to).text(CCC.convertValueToDisplay(fsym, current[key]));
+		} else {
+			$('#' + key + '_' + from + '_' + to).text(current[key]);
+		}
+	}
+
+	$('#PRICE_' + from + '_' + to).removeClass();
+	if (priceDirection & 1) {
+		$('#PRICE_' + from + '_' + to).addClass("up");
+	} else if (priceDirection & 2) {
+		$('#PRICE_' + from + '_' + to).addClass("down");
+	}
+
+	if (from === 'BTC' && to === 'USD')
+		seriesOptions.push({
+			name: current['LASTMARKET_DOGE_USD'],
+			data: [new Date().getTime(), current['PRICE']]
+		});
+
+	if (current['PRICE'] > current['OPEN24HOUR']) {
+		$('#CHANGE24HOURPCT_' + from + '_' + to).removeClass();
+		$('#CHANGE24HOURPCT_' + from + '_' + to).addClass("up");
+	} else if (current['PRICE'] < current['OPEN24HOUR']) {
+		$('#CHANGE24HOURPCT_' + from + '_' + to).removeClass();
+		$('#CHANGE24HOURPCT_' + from + '_' + to).addClass("down");
+	}
+};
+
+function dataUnpack(data) {
+	var from = data['FROMSYMBOL'];
+	var to = data['TOSYMBOL'];
+	var fsym = CCC.STATIC.CURRENCY.getSymbol(from);
+	var tsym = CCC.STATIC.CURRENCY.getSymbol(to);
+	var pair = from + to;
+	console.log(data);
+
+	if (!currentPrice.hasOwnProperty(pair)) {
+		currentPrice[pair] = {};
+	}
+
+	for (var key in data) {
+		currentPrice[pair][key] = data[key];
+	}
+
+	if (currentPrice[pair]['LASTTRADEID']) {
+		currentPrice[pair]['LASTTRADEID'] = parseInt(currentPrice[pair]['LASTTRADEID']).toFixed(0);
+	}
+	currentPrice[pair]['CHANGE24HOUR'] = CCC.convertValueToDisplay(tsym, (currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']));
+	currentPrice[pair]['CHANGE24HOURPCT'] = ((currentPrice[pair]['PRICE'] - currentPrice[pair]['OPEN24HOUR']) / currentPrice[pair]['OPEN24HOUR'] * 100).toFixed(2) + "%";
+	displayData(currentPrice[pair], from, to, tsym, fsym);
+};
+
+var seriesOptions = [];
+
+function square(containerId) {
+	Highcharts.stockChart(containerId, {
+
+		rangeSelector: {
+			selected: 4
+		},
+
+		yAxis: {
+			labels: {
+				formatter: function() {
+					return (this.value > 0 ? ' + ' : '') + this.value + '%';
+				}
+			},
+			plotLines: [{
+				value: 0,
+				width: 2,
+				color: 'silver'
+			}]
+		},
+
+		plotOptions: {
+			series: {
+				compare: 'percent',
+				showInNavigator: true
+			}
+		},
+
+		tooltip: {
+			pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+			valueDecimals: 2,
+			split: true
+		},
+
+		series: seriesOptions
+	});
+}
